692d4d266450340dcbbad4ad28264a06cadeeca9
Thebibii <habibiesanji@gmail.com>
2026-07-16 20:34:34 +0700

    feat(seo): add title metadata for /statistics and /guestbook pages

---
+++ src/app/(guest)/guestbook/layout.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Guestbook",
+};
+
+export default function GuestbookLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children;
+}
---
+++ src/app/(guest)/statistics/layout.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Statistics",
+};
+
+export default function StatisticsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children;
+}
